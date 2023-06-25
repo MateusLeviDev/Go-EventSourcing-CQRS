3bac654501372de231708df4eebd975a3b7b01d1
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-25 03:27:11 -0300

    feat: add problem 2

---
+++ Logic/src/main/java/demo/numberTwo.java
@@ -0,0 +1,20 @@
+//Author: Levi
+//        jun/2023
+
+package demo;
+
+import java.util.Scanner;
+
+public class numberTwo {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int value = scanner.nextInt();
+        double div = value/5;
+
+        System.out.println(div);
+
+        scanner.close();
+    }
+}
