e7b007e686f8aa756d4e5d776930b32f4b603faa
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-25 03:27:21 -0300

    feat: add problem 3

---
+++ Logic/src/main/java/demo/numberThree.java
@@ -0,0 +1,32 @@
+//Author: Levi
+//        jun/2023
+
+package demo;
+
+import java.util.Scanner;
+
+public class numberThree {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe dois valores:");
+        int firstValue = scanner.nextInt();
+        int secondValue = scanner.nextInt();
+        int result = smallerNumber(firstValue, secondValue);
+
+        System.out.println(result);
+
+//        simple way without smallerNumber
+//        int smaller = Math.min(firstValue, secondValue);
+//        System.out.println(smaller);
+
+        scanner.close();
+    }
+
+    public static int smallerNumber(int a, int b) {
+        if(a < b) {
+            return a;
+        } return b;
+    }
+}
