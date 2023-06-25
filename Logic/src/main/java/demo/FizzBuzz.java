c04edd66f155450a5cf12ed274d569f7429da03c
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-25 03:26:47 -0300

    feat: FizzBuzz

---
+++ Logic/src/main/java/demo/FizzBuzz.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100 ; i++) {
+            String result = switch (i % 15) {
+                case 0 -> "FizzBuzz";
+                case 3 -> "Fizz";
+                case 5 -> "Buzz";
+                default -> Integer.toString(i);
+            };
+            System.out.println(result);
+        }
+
+    }
+}
