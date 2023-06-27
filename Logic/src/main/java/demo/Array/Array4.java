9c7d5f7c78d7a1d853d780350c550801530f047e
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-27 20:59:28 -0300

    chore: fixing syntax

--- Logic/src/main/java/demo/Array/Array4.java
+++ Logic/src/main/java/demo/Array/Array4.java
@@ -5,16 +5,19 @@ package demo.Array;
 
 import java.util.Scanner;
 
-public class Array2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] array1 = new int[8];
-        int[] array2 = new int[array1.length];
+public class Array4 {
 
-        for(int i = 0; i<array1.length; i++) {
-            System.out.println("Value: " + i);
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array1 = new int[15];
+        double[] array2 = new double[array1.length];
+
+        for (int i=0; i< array1.length; i++) {
+            System.out.println("Value: " + (i+1));
             array1[i] = scanner.nextInt();
-            array2[i] = array1[i] * 2;
+            array2[i] = Math.sqrt(array1[i]);
         }
 
         System.out.println("First: ");
