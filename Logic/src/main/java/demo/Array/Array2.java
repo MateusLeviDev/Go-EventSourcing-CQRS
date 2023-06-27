c3e8e6ba8367fd8e217919ae67d12a2dbcf1fb4c
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-26 21:59:21 -0300

    feat: array exercises

---
+++ Logic/src/main/java/demo/Array/Array2.java
@@ -0,0 +1,30 @@
+package demo.Array;
+
+import java.util.Scanner;
+
+public class Array2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array1 = new int[8];
+        int[] array2 = new int[array1.length];
+
+        for(int i = 0; i<array1.length; i++) {
+            System.out.println("Value: " + i);
+            array1[i] = scanner.nextInt();
+            array2[i] = array1[i] * 2;
+        }
+
+        System.out.println("First: ");
+        for(int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + ", ");
+        }
+        System.out.println();
+
+        System.out.println("Second: ");
+        for(int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i] + ", ");
+        }
+
+        scanner.close();
+    }
+}
