fc2b82c1aac046d91f6490e2dd056cdc5a74f7dd
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-07-03 19:07:25 -0300

    chore: fixing syntax

--- Logic/src/main/java/demo/Main.java
+++ Logic/src/main/java/demo/Main.java
@@ -3,12 +3,5 @@ package demo;
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            throw new Exc1();
-        } catch (Exc0 e0) {
-            System.out.println("Ex0 caugth");
-        } catch (Exception e) {
-            System.out.println("Exception caught");
-        }
     }
 }
