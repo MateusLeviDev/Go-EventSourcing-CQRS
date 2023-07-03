ad0c61619932c03e6f667c18c29e8bc093732f34
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-07-02 22:48:21 -0300

    chore: added examples

--- Logic/src/main/java/demo/Main.java
+++ Logic/src/main/java/demo/Main.java
@@ -2,6 +2,13 @@ package demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            throw new Exc1();
+        } catch (Exc0 e0) {
+            System.out.println("Ex0 caugth");
+        } catch (Exception e) {
+            System.out.println("Exception caught");
+        }
     }
 }
